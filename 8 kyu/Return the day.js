2401aa34c7af71878ae17d2bda00520f246adcfa
Evgen Orlovski <orloffski@gmail.com>
2023-03-13 02:34:18 +0300

    feat: add 8 kyu tasks

---
+++ 8 kyu/Return the day.js
@@ -0,0 +1,36 @@
+/*
+Complete the function which returns the weekday according to the input number:
+    1 returns "Sunday"
+    2 returns "Monday"
+    3 returns "Tuesday"
+    4 returns "Wednesday"
+    5 returns "Thursday"
+    6 returns "Friday"
+    7 returns "Saturday"
+    Otherwise returns "Wrong, please enter a number between 1 and 7"
+*/
+function whatday(num) { 
+
+  switch(num){
+    case 1:
+        return "Sunday";
+    case 2:
+        return "Monday";
+    case 3:
+        return "Tuesday";
+    case 4:
+        return "Wednesday";
+    case 5:
+        return "Thursday";
+    case 6:
+        return "Friday";
+    case 7: 
+        return "Saturday";
+    default:
+        return "Wrong, please enter a number between 1 and 7";
+  }
+}
+
+for(let i = 1; i <= 8; i++){
+    console.log(whatday(i));
+}
