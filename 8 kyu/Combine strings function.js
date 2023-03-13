8ae43f925b973c0f30f41ef2132a0a5da78ed321
Evgen Orlovski <orloffski@gmail.com>
2023-03-13 17:06:41 +0300

    feat: add 15 tasks to 8 kyu

---
+++ 8 kyu/Combine strings function.js
@@ -0,0 +1,14 @@
+/**
+Create a function named combineNames that accepts two parameters (first and last name). The function should return the full name.
+
+Example:
+
+combineNames('James', 'Stevens')
+returns:
+
+'James Stevens'
+ */
+
+combineNames = (firstName, lastName) => `${firstName} ${lastName}`;
+
+console.log(combineNames('James', 'Stevens'));
