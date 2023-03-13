62d6eb3c912ae4b2d0449d0bdb5df05173d9d8f0
Evgen Orlovski <orloffski@gmail.com>
2023-03-14 01:10:56 +0300

    feat: add 5 tasks to 8 kyu

---
+++ 8 kyu/Printing Array elements with Comma delimiters.js
@@ -0,0 +1,14 @@
+/**
+Input: Array of elements
+
+["h","o","l","a"]
+
+Output: String with comma delimited elements of the array in th same order.
+
+"h,o,l,a"
+
+ */
+
+printArray = (array) => array.toString();
+
+console.log(printArray([2,4,5,2]));
