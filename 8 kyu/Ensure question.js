1ddc86e8e472613e35b8113fc0c01ca8f1a20cff
Evgen Orlovski <orloffski@gmail.com>
2023-03-16 17:08:00 +0300

    feat: add 5 tasks to 8 kyu

---
+++ 8 kyu/Ensure question.js
@@ -0,0 +1,16 @@
+/**
+Given a string, write a function that returns the string with a question mark ("?") appends to the end, 
+unless the original string ends with a question mark, in which case, returns the original string.
+
+For example (Input --> Output)
+
+"Yes" --> "Yes?" 
+"No?" --> "No?"
+
+ */
+
+const ensureQuestion = s => s.endsWith('?') ? s : s + '?';
+
+console.log(ensureQuestion(''));
+console.log(ensureQuestion('Yes'));
+console.log(ensureQuestion('No?'));
