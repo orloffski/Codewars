09a79127b43468964609c91b912f053cc941a206
Evgen Orlovski <orloffski@gmail.com>
2023-03-20 03:47:18 +0300

    feat: add 3 tasks to 7 kyu

---
+++ 7 kyu/Valid Parentheses.js
@@ -0,0 +1,54 @@
+/**
+Write a function that takes a string of parentheses, and determines if the order of the parentheses is valid. 
+The function should return true if the string is valid, and false if it's invalid.
+Examples
+
+"()"              =>  true
+")(()))"          =>  false
+"("               =>  false
+"(())((()())())"  =>  true
+
+Constraints
+
+0 <= length of input <= 100
+
+    All inputs will be strings, consisting only of characters ( and ).
+    Empty strings are considered balanced (and therefore valid), and will be tested.
+    For languages with mutable strings, the inputs should not be mutated.
+
+
+Protip: If you are trying to figure out why a string of parentheses is invalid, paste the parentheses into the code editor, 
+and let the code highlighting show you!
+
+ */
+
+const validParentheses = (parenStr) => {
+    let counter = 0;
+
+    for(let i = 0; i < parenStr.length; i++){
+        if(parenStr[i] == '('){
+            counter++;
+        }else{
+            counter--;
+        }
+
+        if(counter < 0){
+            return false;
+        }
+    }
+
+    return counter == 0 ? true : false;
+}
+
+console.log(validParentheses("()"));
+console.log(validParentheses("((()))"));
+console.log(validParentheses("()()()"));
+console.log(validParentheses("(()())()"));
+console.log(validParentheses("()(())((()))(())()"));
+
+console.log(validParentheses(")("));
+console.log(validParentheses("()()("));
+console.log(validParentheses("((())"));
+console.log(validParentheses("())(()"));
+console.log(validParentheses(")()"));
+console.log(validParentheses(")"));
